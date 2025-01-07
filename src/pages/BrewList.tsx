6ae55d4eef811634a8c45afcbf24491a910150dc
takatama <takatama@users.noreply.github.com>
2025-01-07 19:56:49 +0900

    Remove unused types and clean up BrewList component

--- src/pages/BrewList.tsx
+++ src/pages/BrewList.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Brews } from '../types/Brew';
 import { useBrewContext } from '../context/BrewContext';
 
 const BrewList: React.FC = () => {
