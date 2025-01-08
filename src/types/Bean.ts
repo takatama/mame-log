c8cbd0f6c85d8dec45837d145c6c1e21da574f87
takatama <takatama@users.noreply.github.com>
2025-01-08 12:14:22 +0900

    Refactor BeanForm to use a single state object for improved data management and make Bean properties optional in the type definition

--- src/types/Bean.ts
+++ src/types/Bean.ts
@@ -1,18 +1,18 @@
 export interface Bean {
   id: number;
-  name: string;
-  country: string;
-  area: string;
-  drying_method: string;
-  processing_method: string;
-  roast_level: string;
-  roast_date: string;
-  purchase_date: string;
-  purchase_amount: number; // g
-  price: number; // JPY
-  seller: string;
-  seller_url: string;
-  photo_url: string;
+  name?: string;
+  country?: string;
+  area?: string;
+  drying_method?: string;
+  processing_method?: string;
+  roast_level?: string;
+  roast_date?: string;
+  purchase_date?: string;
+  purchase_amount?: number; // g
+  price?: number; // JPY
+  seller?: string;
+  seller_url?: string;
+  photo_url?: string;
   notes?: string;
-  is_active: boolean;
+  is_active?: boolean;
 }
